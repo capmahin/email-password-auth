6aff50ef771506f260188f29f98126cf8be51ddc
capmahin <mahinulhuque1996@gmail.com>
2022-04-07 05:50:18 +0600

    add react bootstrap and add a login from

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { getAuth } from "firebase/auth";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import app from "./firebase.init";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const auth = getAuth(app);
 
@@ -18,14 +21,36 @@ function App() {
     event.preventDefault();
   };
   return (
-    <div className="App">
-      <form onSubmit={handleFromSubmit}>
-        <input onBlur={handleEmailBlur} type="email" name="" id="" />
-        <br />
-        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
-        <br />
-        <input type="submit" value="Login" />
-      </form>
+    <div>
+      <div className="registration w-50 mx-auto mt-5">
+        <h2 className="text-primary">Please Register!! </h2>
+        <Form onSubmit={handleFromSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              onBlur={handleEmailBlur}
+              type="email"
+              placeholder="Enter email"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              onBlur={handlePasswordBlur}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
